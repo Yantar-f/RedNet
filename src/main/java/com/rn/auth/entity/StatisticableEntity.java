abf3f69e9392da0082db3f001fc45f832348db7a
Yantar-f <shevchenko9542@gmail.com>
2023-01-29 17:17:23 +0600

    Added: Statisticable entity

---
+++ src/main/java/com/rn/auth/entity/StatisticableEntity.java
@@ -0,0 +1,27 @@
+package com.rn.auth.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.util.Date;
+
+@MappedSuperclass
+public class StatisticableEntity {
+
+    @Column(name = "status")
+    @Enumerated(EnumType.STRING)
+    private EnumStatus status;
+
+    @Column(name = "created_date")
+    @CreatedDate
+    private Date createdDate;
+
+    @Column(name = "last_modified_date")
+    @LastModifiedDate
+    private Date lastModifiedDate;
+
+}
