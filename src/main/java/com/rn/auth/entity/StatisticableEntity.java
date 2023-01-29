3c9223aef63401569472d06264dda1c8039c8b20
Yantar-f <shevchenko9542@gmail.com>
2023-01-29 20:17:12 +0600

    Modified: Getters and Setters for StatisticableEntity fields

--- src/main/java/com/rn/auth/entity/StatisticableEntity.java
+++ src/main/java/com/rn/auth/entity/StatisticableEntity.java
@@ -27,4 +27,27 @@ public class StatisticableEntity {
     @LastModifiedDate
     private Date lastModifiedDate;
 
+    public EnumStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(EnumStatus status) {
+        this.status = status;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Date getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
 }
