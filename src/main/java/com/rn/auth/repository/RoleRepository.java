f0f64a93f4770b5c34f08d95ea0f0e183c9bdecf
Yantar-f <shevchenko9542@gmail.com>
2023-02-16 07:46:36 +0600

    Modified: repositories

--- src/main/java/com/rn/auth/repository/RoleRepository.java
+++ src/main/java/com/rn/auth/repository/RoleRepository.java
@@ -1,13 +1,14 @@
 package com.rn.auth.repository;
 
-import com.rn.auth.model.EnumRole;
-import com.rn.auth.model.Role;
+import com.rn.auth.model.entity.EnumRole;
+import com.rn.auth.model.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface RoleRepository {
+public interface RoleRepository extends JpaRepository<Role, Long> {
     Optional<Role> findByDesignation(EnumRole designation);
 
     Boolean existsByDesignation(EnumRole designation);
--- src/main/java/com/rn/auth/repository/UserRepository.java
+++ src/main/java/com/rn/auth/repository/UserRepository.java
@@ -1,6 +1,6 @@
 package com.rn.auth.repository;
 
-import com.rn.auth.model.User;
+import com.rn.auth.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,9 +9,5 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
-    Optional<User> findByEmail(String email);
-
     Boolean existsByUsername(String username);
-
-    Boolean existsByEmail(String email);
 }
