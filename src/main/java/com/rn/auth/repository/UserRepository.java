48c5f7c052bddd8aad1e0fee77d333bb13cb7f46
yantar <shevchenko9542@gmail.com>
2023-03-14 21:38:31 +0600

    Added: entity grath to findbyusername method

--- src/main/java/com/rn/auth/repository/UserRepository.java
+++ src/main/java/com/rn/auth/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.rn.auth.repository;
 
 import com.rn.auth.entity.User;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +9,7 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    @EntityGraph(attributePaths = {"roles"})
     Optional<User> findByUsername(String username);
     boolean existsByUsernameOrEmail(String username, String email);
 }
