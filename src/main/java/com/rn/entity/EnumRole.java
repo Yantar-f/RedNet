49742dec60a4f2c3d7c4c44759405646a365ee67
Yantar-f <77274156+Yantar-f@users.noreply.github.com>
2023-03-25 19:49:44 +0600

    Revert "Sec auth"

--- src/main/java/com/rn/entity/EnumRole.java
+++
@@ -1,6 +0,0 @@
-package com.rn.entity;
-
-public enum EnumRole {
-    USER,
-    ADMIN
-}
--- src/main/java/com/rn/entity/EnumStatus.java
+++
@@ -1,7 +0,0 @@
-package com.rn.entity;
-
-public enum EnumStatus {
-    ACTIVE,
-    INACTIVE,
-    DELETED,
-}
