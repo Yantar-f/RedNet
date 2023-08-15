3fa304007f9c81b1ddd16cfc0bc218ab2a85cd47
Матвей Шевченко <shevchenko9542@gmail.com>
2023-08-15 13:00:50 +0600

    diagram change

--- README.md
+++ README.md
@@ -3,15 +3,15 @@ Social network for data exchange, discussions, organization of events and commun
 
 
 ## Project Description
-in progres...
+in progress...
 ## Get Started Instruction
-in progres...
+in progress...
 ## Project Design
-![app-diagram](rednet-system-design.png)
+![app-diagram](system-design-diagram.svg)
 
 ## Project Modules Repositories
 * [api-gateway](https://github.com/Yantar-f/RedNet-api-gateway)
-* [user-management-service](https://github.com/Yantar-f/RedNet-user-management-service)
+* [auth-management-service](https://github.com/Yantar-f/RedNet-auth-management-service)
 * [sse-service](https://github.com/Yantar-f/RedNet-sse-service)
 * [chat-service](https://github.com/Yantar-f/RedNet-chat-service)
 
@@ -19,11 +19,10 @@ in progres...
 BackEnd stack:
 
 [![Java](https://img.shields.io/badge/java-%23ED8B00.svg?style=for-the-badge&logo=java&logoColor=white)](https://www.java.com/)
-[![Spring](https://img.shields.io/badge/spring-%236DB33F.svg?style=for-the-badge&logo=spring&logoColor=white)](https://spring.io/)
 [![Postgres](https://img.shields.io/badge/postgres-%23316192.svg?style=for-the-badge&logo=postgresql&logoColor=white)](https://www.postgresql.org/)
 [![ApacheCassandra](https://img.shields.io/badge/cassandra-%231287B1.svg?style=for-the-badge&logo=apache-cassandra&logoColor=white)](https://cassandra.apache.org/)
 [![Redis](https://img.shields.io/badge/redis-%23DD0031.svg?style=for-the-badge&logo=redis&logoColor=white)](https://www.redis.io/)
 [![RabbitMQ](https://img.shields.io/badge/Rabbitmq-FF6600?style=for-the-badge&logo=rabbitmq&logoColor=white)](https://www.rabbitmq.com/)
 
 FrontEnd stack:
-in progres...
+in progress...
---
+++ system-design-diagram.py
@@ -0,0 +1,62 @@
+from diagrams import Diagram, Cluster, Edge
+from diagrams.onprem.client import User
+from diagrams.onprem.database import Postgresql, Cassandra
+from diagrams.onprem.inmemory import Redis
+from diagrams.onprem.queue import RabbitMQ
+from diagrams.programming.framework import Spring
+
+with Diagram(
+    name="RedNet System Design",
+    filename="system-design-diagram",
+    outformat="svg",
+    show=True,
+    direction="TB"
+):
+    client_request_edge = Edge(color="#0ECC00")
+    client_proxied_request_edge = Edge(color="#0ECC00", style="dashed")
+    server_to_server_edge = Edge(color="#00DEDB")
+
+    client = User("spa")
+    agw = Spring("agw")
+
+    with Cluster("External Services"):
+        with Cluster("Auth Management Service"):
+            auth_logic_server = Spring("bl server")
+            accounts_db = Postgresql("accounts")
+            refresh_tokens_db = Redis("refresh tokens")
+
+        with Cluster("Sse Service"):
+            sse_logic_server = Spring("bl server")
+
+        with Cluster("Chat Service"):
+            chat_logic_server = Spring("bl server")
+            messages_db = Cassandra("messages")
+
+    with Cluster("Internal Services"):
+        notification_queue = RabbitMQ("notification queue")
+
+        with Cluster("Session Service"):
+            session_logic_server = Spring("bl server")
+            sessions_db = Cassandra("sessions")
+
+    client_service_group = [
+        auth_logic_server,
+        sse_logic_server,
+        chat_logic_server
+    ]
+
+    notifiable_service_group = [
+        auth_logic_server,
+        session_logic_server,
+        chat_logic_server
+    ]
+
+    client >> client_request_edge >> agw >> client_proxied_request_edge >> client_service_group
+    auth_logic_server >> server_to_server_edge >> session_logic_server
+    auth_logic_server >> accounts_db
+    auth_logic_server >> refresh_tokens_db
+    chat_logic_server >> messages_db
+    session_logic_server >> sessions_db
+    notifiable_service_group >> server_to_server_edge >> notification_queue
+    notification_queue >> server_to_server_edge >> sse_logic_server
+
